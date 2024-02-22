aef9ee69bb5e2ad957f1515649be07721af6ecbc
Emilio-Plances <emilio.plances@gmail.com>
2024-02-22 22:57:54 +0100

    Exceptions and Responses

---
+++ src/main/java/com/example/GestioneEventi/exceptions/ErrorHandler.java
@@ -0,0 +1,26 @@
+package com.example.PG.s.Dragons.exceptions;
+
+import com.example.PG.s.Dragons.responses.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse notFoundException(NotFoundException e){
+        return new ErrorResponse(e.getMessage());
+    }
+    @ExceptionHandler(UnauthorizedException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorResponse unauthorizedException(UnauthorizedException e){
+        return new ErrorResponse(e.getMessage());
+    }
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse badRequestException(BadRequestException e){
+        return new ErrorResponse(e.getMessage());
+    }
+}
